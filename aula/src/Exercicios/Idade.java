4a69b36a0b2e8681fea40d948a5e5c7e71bec332
Lucas Monteiro <lucmont00@gmail.com>
2023-03-06 19:42:24 -0300

    Update Idade.java

--- aula/src/Exercicios/Idade.java
+++ aula/src/Exercicios/Idade.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Idade {
 	public static void main(String args[]) {
 		int idade;
-		System.out.println("Idade\n");
+		System.out.println("Idade?\n");
 		Scanner sc = new Scanner(System.in);
 		idade = sc.nextInt();
 		if (idade > 17)
